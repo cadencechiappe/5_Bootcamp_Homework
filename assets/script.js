f77076c9b1a263e9b6781e77d727433cf989a347
Cadence Chiappe <cadence@chiappe.com>
2022-04-16 10:12:02 -0500

    modified css and html

---
+++ assets/script.js
@@ -0,0 +1,37 @@
+// to display the current date
+var currentDayEl = moment().format("dddd, MMMM Do");
+$("#currentDay").text(currentDayEl);
+
+// time selectors for time cards
+var eightAmEl = moment("8" , "h").format("h a");
+$("#eightAm").text(eightAmEl);
+
+var nineAmEl = moment("9" , "h").format("h a");
+$("#nineAm").text(nineAmEl);
+
+var tenAmEl = moment("10" , "h").format("h a");
+$("#tenAm").text(tenAmEl);
+
+var elevenAmEl = moment("11" , "h").format("h a");
+$("#elevenAm").text(elevenAmEl);
+
+var twelvePmEl = moment("12" , "h").format("h a");
+$("#twelvePm").text(twelvePmEl);
+
+var onePmEl = moment("13" , "h").format("h a");
+$("#onePm").text(onePmEl);
+
+var twoPmEl = moment("14" , "h").format("h a");
+$("#twoPm").text(twoPmEl);
+
+var threePmEl = moment("15" , "h").format("h a");
+$("#threePm").text(threePmEl);
+
+var fourPmEl = moment("16" , "h").format("h a");
+$("#fourPm").text(fourPmEl);
+
+var fivePmEl = moment("17" , "h").format("h a");
+$("#fivePm").text(fivePmEl);
+
+var sixPmEl = moment("18" , "h").format("h a");
+$("#sixPm").text(sixPmEl);
